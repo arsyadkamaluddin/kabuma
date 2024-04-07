72165daf6238a064de7d89e0f689bfb4ceda246b
d-arsya <kamaluddin.arsyad17@gmail.com>
2024-04-07 11:30:27 +0700

    feby added

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const nama = document.querySelectorAll(".nama img")
-const quot = document.querySelector(".quot")
+const quot = document.querySelector(".quot p")
 
 nama.forEach(function(e){
     e.addEventListener("mouseover",function(f){
         quot.style.animation = "myAnim 1.5s ease 0s 1 normal forwards;"
-        quot.innerHTML=f.srcElement.nextElementSibling.innerHTML
+        quot.innerText=f.srcElement.nextElementSibling.innerHTML
         // quot.style.animation = ""
     })
 })
